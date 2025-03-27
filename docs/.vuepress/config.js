9cc20b988b3e55d2fafd5c7f82e9f1039c47ca8f
小航love666 <1531137510@qq.com>
2025-03-27 12:09:24 +0800

    fix(doc): fix base_url.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     theme: 'vdoing', // 使用依赖包主题
     title: "Leetcode-runner",
     description: '为摸鱼而生',
-    // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+    base: '/leetcode-runner-doc/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
     head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
         ['link', {rel: 'icon', href: '/img/logo/leetcode-runner-v1.png'}], //favicons，资源放在public文件夹
         ['meta', {name: 'keywords', content: 'leetcode,Leetcode-runner,力扣'}],
