8a828870fad355e578b7ba008eeef709e85d9a7f
小航love666 <1531137510@qq.com>
2025-03-30 23:24:57 +0800

    feat(doc): 补充算法模板和导航

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -8,7 +8,7 @@ module.exports = [
             {
                 text: '更新日志', items: [
                     {text: 'v3.5.0', link: '/pages/1705234447153963010/'},
-                    {text: '素材', link: '/pages/1705234447153963010/'},
+                    {text: '素材', link: '/pages/b45deb/'},
                 ]
             },
         ]
@@ -16,6 +16,11 @@ module.exports = [
     {
         text: '教程', link: '/pages/a20ce8/', items: [
             {text: '使用配置', link: '/pages/a20ce8/'},
+            {
+                text: '算法', items: [
+                    {text: '算法模板', link: '/pages/7f76b7/'},
+                ]
+            },
         ]
     },
     {text: '问答', link: '/pages/9cc27d/'},
