346e873576feebbd0164642bf4f9bfecbb3d68f0
xuhuafeifei <2508020102@qq.com>
2025-04-02 22:09:35 +0800

    chore: 更新文档和图片资源
    - 新增多个图片资源，用于文档展示
    - 更新文档内容，包括快速上手、使用配置、辅助功能等章节
    - 新增开发文档，详细介绍了项目架构、模块设计和功能实现
    - 修复部分文档中的错误和格式问题

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -4,19 +4,28 @@ module.exports = [
         text: '指南', link: '/pages/a2f161/', items: [
             {text: '简 介', link: '/pages/a2f161/'},
             {text: '安 装', link: '/pages/2f674a/'},
-            {text: '快速开始', link: '/pages/793dcb/'},
+            {
+                text: '开 始', items: [
+                    {text: '快速上手', link: '/pages/793dcb/'},
+                    {text: '使用配置', link: '/pages/a20ce8/'},
+                    {text: '辅助功能', link: '/pages/98u9hk/'}
+                ]
+            },
             {
                 text: '更新日志', items: [
                     {text: 'v3.6.9', link: '/pages/ojodioj/'},
                     // {text: '素材', link: '/pages/b45deb/'},
                 ]
             },
+
         ]
     },
     {
         text: '教程', link: '/pages/a20ce8/', items: [
-            {text: '源码讲解', link: '/pages/a55945553/', items: [
-                {text: '调试引擎', link: '/pages/a55945553/'},
+            {text: '开发文档-1', link: '/pages/e88d84d/'},
+            {text: '开发文档-2', link: '/pages/yih7ij/'},
+            {text: '调试引擎', items: [
+                // {text: '调试引擎', link: '/pages/a55945553/'},
             ]},
             {
                 text: '算法', items: [
